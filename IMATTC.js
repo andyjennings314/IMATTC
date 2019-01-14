aec45ff81d55821a506d4d54aab735829cc70c85
andyjennings314 <andyjennings314@gmail.com>
2019-01-14 22:50:19 +0000

    UI fixes to mission table borders and action buttons
    Table borders in mission panels now use panel text colour, action buttons have different text label, and on Published missions have matching colour scheme with hover behaviour. TODO: Port same UI fix to other mission panel states

--- IMATTC.js
+++ IMATTC.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IMATTC
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.3.2
 // @description  A usability overhaul for the Ingress Mission Authoring Tool
 // @author       You
 // @match        https://mission-author-dot-betaspike.appspot.com/
@@ -24,22 +24,36 @@ $(function () {
 	newCssRules += ".mission-header-container div:nth-of-type(1){padding-right: 5px; width: 60px}";
 	newCssRules += ".mission-header-container div:nth-of-type(2){width: calc(100% - 115px)}";
 	newCssRules += ".mission-header-container div:nth-of-type(3){padding-left: 5px; width: 45px}";
-	newCssRules += ".action-button .caret 						{border-bottom-color: #5afbea;}"
+	newCssRules += ".button, button 							{background-image: none;}";
 	newCssRules += ".mission .name.glyphicon 					{font-size: 40px;}"
 	newCssRules += ".mission .name:not(.glyphicon) 				{text-align: center; display: block;}"
 	
 	newCssRules += ".mission-list-item-published 				{background-image: none; background: darkgreen;color: lightgreen;}";
 	newCssRules += ".list .mission .mission-title-published 	{color: lightgreen;}";
+	newCssRules += ".mission-list-item-published .table-bordered * {border-color: lightgreen;}";
+	newCssRules += ".mission-list-item-published .button		{color: lightgreen; border-color: lightgreen; background-color: #004d00;}";
+	newCssRules += ".mission-list-item-published .button:hover	{background-color: #003300;}";
+	newCssRules += ".mission-list-item-published .button .caret	{border-bottom-color: lightgreen;}"
+	
 	newCssRules += ".mission-list-item-draft 					{background-image: none; background: #a42e12;color: #f7ba5f;}";
 	newCssRules += ".list .mission .mission-title-draft 	{color: #f7ba5f;}";
+	newCssRules += ".mission-list-item-draft .table-bordered * {border-color: #f7ba5f;}";
+	
 	newCssRules += ".mission-list-item-draft_of_published_mission {background-image: none; background: olive;color: greenyellow;}";
 	newCssRules += ".list .mission .mission-title-draft_of_published_mission 	{color: greenyellow;}";
+	newCssRules += ".mission-list-item-draft_of_published_mission .table-bordered * {border-color: greenyellow;}";
+	
 	newCssRules += ".mission-list-item-submitted				{background-image: none; background: darkgoldenrod;color: gold;}";
 	newCssRules += ".list .mission .mission-title-submitted 	{color: gold;}";
+	newCssRules += ".mission-list-item-submitted .table-bordered * {border-color: gold;}";
+	
 	newCssRules += ".mission-list-item-disabled 				{background-image: none; background: #6b6b6b;color: red;}";
 	newCssRules += ".list .mission .mission-title-disabled	 	{color: red;}";
+	newCssRules += ".mission-list-item-disabled .table-bordered * {border-color: red;}";
+	
 	newCssRules += ".mission-list-item-submitted_and_published	{background-image: none; background: olivedrab;color: springgreen;}";
 	newCssRules += ".list .mission .mission-title-submitted_and_published 	{color: springgreen;}";
+	newCssRules += ".mission-list-item-submitted_and_published .table-bordered * {border-color: springgreen;}";
 	
 	newCssRules += ".dropup 									{position: relative;}";
 	newCssRules += ".dropup .dropdown-menu						{top: initial; bottom: 30px; left: 0; right: 0; text-align: center;}";
@@ -157,7 +171,7 @@ function init(){
 					   newMissionPanel += "<td>" + mission.stats.num_completed + "</td>";					   
 					}
 				   newMissionPanel += "</tr></table>";
-				   newMissionPanel += "<div class='dropup'><button class='button action-button dropdown-toggle' type='button' id='dropdownMenu1' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>Action <span class='caret'></span></button>";
+				   newMissionPanel += "<div class='dropup'><button class='button action-button dropdown-toggle' type='button' id='dropdownMenu1' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>Perform Mission Action <span class='caret'></span></button>";
 				   newMissionPanel += "<ul class='dropdown-menu' aria-labelledby='dropdownMenu1'>"
 				   newMissionPanel += "<li><a role='button' ng-click='button1Clicked(missions["+i+"])'>" + missionScope.getButton1Title(mission) + "</a></li>";
 				   newMissionPanel += "<li><a role='button' ng-click='button2Clicked(missions["+i+"])'>" + missionScope.getButton2Title(mission) + "</a></li>";
