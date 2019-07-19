faa393ddcc93802134d3b89fa5a5e0f4ee657ae2
andyjennings314 <andyjennings314@gmail.com>
2019-07-19 22:08:41 +0100

    Updated readme.md, +v#

--- IMATTC.user.js
+++ IMATTC.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         IMATTC
-// @version      1.5.4
+// @version      1.6.0
 // @description  A usability overhaul for the Ingress Mission Authoring Tool
 // @author       @Chyld314
 // @match        https://mission-author-dot-betaspike.appspot.com/
