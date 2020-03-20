56d5079b7d85df7d311637d5737efc567471608b
andyjennings314 <andyjennings314@gmail.com>
2020-03-20 14:13:09 +0000

    Added new missions.ingress.com domain

--- IMATTC.user.js
+++ IMATTC.user.js
@@ -1,10 +1,14 @@
 // ==UserScript==
 // @name         IMATTC
-// @version      1.7.1
+// @version      1.7.2
 // @description  A usability overhaul for the Ingress Mission Authoring Tool
 // @author       @Chyld314
 // @match        https://mission-author-dot-betaspike.appspot.com/
 // @match        https://mission-author-dot-betaspike.appspot.com/edit*
+// @match        http://missions.ingress.com/
+// @match        http://missions.ingress.com/edit*
+// @match        https://missions.ingress.com/
+// @match        https://missions.ingress.com/edit*
 // @require      https://code.jquery.com/ui/1.10.4/jquery-ui.min.js
 // @grant        none
 // @downloadURL	 https://github.com/andyjennings314/IMATTC/raw/master/IMATTC.user.js
