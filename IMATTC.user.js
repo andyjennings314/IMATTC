f1a276c0f150d07ac97659bef9984d960cecca69
andyjennings314 <andyjennings314@gmail.com>
2020-07-24 22:01:15 +0100

    Version number

--- IMATTC.user.js
+++ IMATTC.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         IMATTC
-// @version      1.7.4
+// @version      1.8.0
 // @description  A usability overhaul for the Ingress Mission Authoring Tool
 // @author       @Chyld314
 // @match        https://mission-author-dot-betaspike.appspot.com/
